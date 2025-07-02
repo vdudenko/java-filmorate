b5f893bedebd58c8647f1418156341eeefde09e2
vdudenko <dr.vadimlj@yandex.com>
2025-07-02 16:08:48 +0300

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -28,10 +28,10 @@ public class FilmController {
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
         log.info("Создание фильма начинается");
-        if (checkReleaseDate(film.getReleaseDate())) {
-            log.error("Дата появления кино 1895-12-28");
-            throw new ValidationException("Дата появления кино 1895-12-28");
-        }
+        //if (checkReleaseDate(film.getReleaseDate())) {
+            //log.error("Дата появления кино 1895-12-28");
+            //throw new ValidationException("Дата появления кино 1895-12-28");
+    //}
         if (isFilmExist(film)) {
             log.error("Фильма с таким название уже есть");
             throw new DuplicatedDataException("Фильма с таким название уже есть");
